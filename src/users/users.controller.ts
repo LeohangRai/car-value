4ab63240fd3773c996f60bee328b965f9d2932d0
Anup Rai <anup.rai@ekbana.info>
2022-11-19 17:32:08 +0545

    feat: validation pipe setup, signup page wip

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {}
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
+    @Post('signup') 
+    createUser(@Body() body: CreateUserDto) {
+        console.log("body:", body);
+    }
+}
