42ef9cb5061650d6ac2e922ced65022f819a4b0c
Anup Rai <anup.rai@ekbana.info>
2022-12-21 18:22:58 +0545

    feat: one-to-many association between User and Report entities established

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,5 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from 'src/users/user.entity';
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
@@ -25,4 +25,7 @@ export class Report {
 
   @Column()
   milesTravelled: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -4,8 +4,10 @@ import {
   PrimaryGeneratedColumn,
   AfterInsert,
   AfterUpdate,
+  OneToMany,
   AfterRemove
 } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
 
 @Entity()
 export class User {
@@ -17,6 +19,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   printCreatedUserId() {
