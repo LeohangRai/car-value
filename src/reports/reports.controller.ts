2883b1bb9b3976476288e1f4519883dfa6af6646
Anup Rai <anup.rai@ekbana.info>
2022-12-22 13:59:10 +0545

    feat: save the associated user data while creating reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,13 +2,16 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
