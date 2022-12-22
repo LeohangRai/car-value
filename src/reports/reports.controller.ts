bb6081c680f0112efecbbfd669d0614744d2ba7f
Anup Rai <anup.rai@ekbana.info>
2022-12-22 14:47:42 +0545

    feat: serialize, transform; only show the userId while responding with the report entity in POST /reports routes

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from '../interceptors/serialize.intercept';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
